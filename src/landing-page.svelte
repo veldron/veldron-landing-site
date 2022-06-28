ae616d34cb8b1fc7613371b91c5fff1fb49686e9
Palak Gupta <palakgupta120898@gmail.com>
2022-06-28 21:44:44 +0530

    this is veldron landing_page

--- src/landing-page.svelte
+++ src/landing-page.svelte
@@ -1,3 +1,373 @@
+<nav class="flex sticky top-0 bg-gray-100 shadow-lg w-full z-10 items-center justify-between px-24 py-8 max-w-screen-xl mx-auto">
+  <p  class="text-4xl font-bold tracking-wide">Veldron</p>
+
+  <ul class="flex items-center font-bold text-2xl space-x-8">
+    <li class="hover:text-emerald-500" >Home</li>
+    <li class="hover:text-emerald-500">Feature</li>
+    <li class="hover:text-emerald-500">Client</li>
+    <li class="hover:text-emerald-500">Price</li>
+    <li class="hover:text-emerald-500">Contact</li>
+
+  </ul>    
+</nav>
 <div class="container max-w-lg w-full">
-  <p class="text-red-500 text-7xl text-center">Add your content here and remove this line</p>
-</div>
+
+
+    
+      <!-- hero page -->
+      <div class="bg-emerald-100">
+        <div class=" flex flex-col items-center	py-32 px-52">
+          <button class=" bg-emerald-500 hover:bg-emerald-700  items-center text-white font-bold py-3 px-14 rounded-full">
+            Get Started
+          </button>
+          <h1 class="text-black-500 text-6xl font-bold mt-16 text-center">
+            Powerfull Analytics Tools For Your Business.
+          </h1>
+          <span class="text-center text-slate-500 text-xl mt-2">It is a long established fact that a reader by the readable content of a page when looking at its layout.</span>
+          <div>
+            <div class=" ">
+              <div class="container  flex justify-center mt-20 items-center">
+                  <div class="relative"> 
+                      <div class="absolute top-4 left-3">
+                        <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
+                           
+                      </div>
+                    <input type="text" class="h-14 w-96 pl-10 pr-20 rounded-full  shadow-xl border-gray-900 z-0 focus:shadow focus:outline-none" placeholder="Email.......">
+                      <div class="absolute top-2 right-2">
+                        <button class=" bg-emerald-500 hover:bg-emerald-700  text-white font-bold py-2 px-5 rounded-full">
+                          Subcribe
+                        </button>
+                          
+                          
+                      </div>
+                  </div>
+              </div>
+   </div>
+          </div>
+        </div> 
+      </div>
+      
+
+    <div>
+      <img src="https://oliver-tailwind.netlify.app/assets/images/home_side.png" alt="">
+    </div>
+    <div>
+      <h1 class="text-black-500 py-12 px-10 text-5xl font-bold text-center">Explore About Our Awesome Features
+      </h1>
+      <p class=" text-slate-500 text-xl text-center"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eum modi, adipisci facilis.</p>
+    </div>
+    <div class="flex flex-row justify-between py-9 ">
+      <img src="https://oliver-tailwind.netlify.app/assets/images/clients/1.png" alt="">
+      <img src="https://oliver-tailwind.netlify.app/assets/images/clients/2.png" alt="">
+      <img src="https://oliver-tailwind.netlify.app/assets/images/clients/3.png" alt="">
+      <img src="https://oliver-tailwind.netlify.app/assets/images/clients/4.png" alt="">
+    </div>
+
+    <div class="flex flex-row ">
+      <div>
+          <h2 class="text-black-500 text-xl font-bold py-12 text-left"> PERFECT DESIGN</h2>
+          <h1 class=" text-black-500 py-12 px-10 font-bold text-4xl text-center">  It’s Everything You’ll Ever Need.</h1>
+          <p class=" text-slate-500 text-left text-xl "> Create custom landing pages with Landik that converts more visitors than any website. With lots of unique blocks, you can easily build a page without coding.</p>
+          
+         
+          <div class="flex flex-row items-center justify-center mt-8">
+            <button class=" bg-emerald-500 hover:bg-emerald-700  text-white font-bold py-2 px-4 rounded-full">
+              Explore
+            </button>
+          </div>
+      </div>
+      <div>
+        <img src="https://oliver-tailwind.netlify.app/assets/images/features-one.png" alt="">
+      </div>
+    </div>
+    <div>
+      <div>
+        <div class="flex py-16 ">
+       <div>
+         <img src="https://oliver-tailwind.netlify.app/assets/images/features-two.png" alt="">
+       </div>
+       <div>
+         <h3 class="text-black-500 text-xl font-bold py-12 text-left">USER INTERFACE</h3>
+         <h1 class="text-black-500 text-4xl font-bold text-center">Unlimited Features Awaiting For You.</h1>
+           <span class=" text-xl text-zinc-500   py-4 font-bold ">
+             Create custom landing pages with Landik that converts more visitors than any website. With lots of unique blocks, you can easily build a page without coding</span>
+             <div class="flex flex-row items-center justify-center mt-8">
+              <button class="flex flex-row  bg-emerald-500 hover:bg-emerald-700 items-center text-white font-bold py-2 px-4 rounded-full">
+                Explore
+              </button>
+            </div>
+       </div>
+     </div>
+    </div>
+    </div>
+    <div class="bg-emerald-500 p-12  rounded-lg">
+    <h1 class="text-white  text-7xl text-center ">What We Do</h1>  
+    <p class="text-white   text-xl text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eum modi, adipisci facilis.</p>
+    <div class="grid grid-cols-2 gap-2 bg-white py-2 px-3 mt-12">
+      <div class="flex flex-col items-center">
+        <div class="flex flex-row justify-center w-full">
+          <img src="https://oliver-tailwind.netlify.app/assets/images/icon/pen&ruller.svg" alt="">
+          <h2 class="text-black-500 font-bold text-xl text-center">Well Documented</h2>
+        </div>
+        <p class="text-black-500  text-zinc-500 text-xl text-center">Ned ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.
+        </p>
+      </div>
+      <div>
+        <img src="https://oliver-tailwind.netlify.app/assets/images/icon/code.svg" alt="">
+        <h2 class="text-black-500 font-bold text-xl text-center"> Highly Customizable</h2>
+        <p class="text-black-500  text-zinc-500 text-xl text-center">The point of using Lorem Ipsum is has a normal distribution of letters, as opposed to using.
+        </p>
+      </div>
+    
+      <div>
+        <h2 class="text-black-500 font-bold text-xl text-center">24/7 Support</h2>
+        <img src="https://oliver-tailwind.netlify.app/assets/images/icon/headphones.svg" alt="">
+   
+          <span class="text-black-500  text-zinc-500 text-xl text-center"> There are many variations of but the majority have suffered alteration in some form.</span>
+      </div>
+      <div class="">
+        <img src="https://oliver-tailwind.netlify.app/assets/images/icon/groupchat.svg" alt="">
+        <h1 class="text-black-500 font-bold text-xl text-center">Community</h1>
+        <span  class="text-black-500  text-zinc-500 text-xl text-center">If you are going to use a Ipsum, you need to be sure there isn't anything in the middle of text.
+        </span>
+      </div>
+    </div>
+    </div>
+    <div  class=" items-center	py-44 px-52">
+      <h1 class="text-black-500 font-bold text-5xl text-center">Hosts Like You, Worldwide   </h1>
+      <p class="text-xl text-zinc-500 mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eum modi, adipisci facilis.
+      </p>
+      <!-- Rating cards -->
+      <div class="flex flex-row items-center justify-center space-x-16 ">
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="  border-2 p-12">
+            <div class="font-bold text-xl mb-2"> 4.92/5</div>
+            <p class="text-gray-700 text-base"> Average ratings</p>
+          </div>
+        </div>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="border-2 p-12">
+            <div class="font-bold text-xl mb-2">260K+</div>
+            <p class="text-gray-700 text-base">Monthly Installs</p>
+          </div>
+        </div>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="flex flex-col items-center justify-center border-2 p-12">
+            <div class="font-bold text-xl mb-2">40K+</div>
+            <p class="text-gray-700 text-base">Weekly Downloads</p>
+          </div>
+        </div>
+        <div class="max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="flex flex-col items-center justify-center border-2 p-12">
+            <div class="font-bold text-xl mb-2">4.9M</div>
+            <p class="text-gray-700 text-base">Total Downloads</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-emerald-500 py-40  rounded-lg">
+        <h1 class=" text-white text-7xl font-bold text-center">We’re Ready for a Challenging Project
+        </h1>
+        <p class="text-white text-black-500 py-10 text-xl text-center">The point of using Lorem Ipsum is that opposed to using 'Content here, content here', making it look like readable English.</p>
+        <div class="flex flex-row w-full  items-center justify-center">
+          <button  class=" max-w-sm   bg-white font-bold text-black-500 py-2 px-4 rounded-full">
+          start trial free
+        </button>
+    </div>
+    </div>
+    <div class=" py-20 ">
+      <h1 class="font-bold text-5xl text-center"> How Easy Is It To Get Started?</h1>
+      <p class=" text-slate-500 py-10  text-xl text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eum modi, adipisci facilis.
+      </p>
+      <div class="flex flex-row items-center justify-center py-4 px-6 pl-9 space-x-10">
+        <div class="">
+          <button class="bg-emerald-500 hover:bg-emerald-700   items-center text-white font-bold py-3 px-14 rounded-full">STEP 1</button>
+          <h1 class="font-bold text-2xl py-3">Design & Devloping</h1>
+          <p class=" text-slate-500 text-xl ">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        
+        </div>
+        <div>
+          <button class="bg-emerald-500 hover:bg-emerald-700   items-center text-white font-bold py-3 px-14 rounded-full">Step2</button>
+          <h1 class="font-bold text-2xl py-3">Testing Project</h1>
+          <p class=" text-slate-500 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+        <div>
+          <button class="bg-emerald-500 hover:bg-emerald-700   items-center text-white font-bold py-3 px-14 rounded-full"> Step3</button>
+          <h1 class="font-bold text-2xl py-3"> Publisher Project</h1>
+          <p class=" text-slate-500 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+    <div class="py-56">
+      <h1 class="font-bold text-5xl  text-center"> Oliver Is Loved By Users</h1>
+      <p class=" text-slate-500 text-black-500 py-10 text-2xl text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eum modi, adipisci facilis.</p>
+    <!-- create cards -->
+    </div>
+    <div>
+      <h1 class="font-bold text-5xl text-center"> Find A Plan That Works For You</h1>
+      <p class=" text-slate-500 py-10  text-xl text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eum modi, adipisci facilis.
+      </p>
+      <div class=" space-x-12 ">
+        <div class="flex flex-row">
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div class="px-20 py-20">
+              <div class="font-bold text-3xl mb-2">Basic</div>
+              <p class="text-gray-700 py-16 text-xl ">Start building beautiful websites. </p>
+              <button class="bg-emerald-500 hover:bg-emerald-700  items-center text-white font-bold py-3 px-14 rounded-full">
+                Choose Now
+              </button>
+              <div class=" py-6 ">
+                <h1  class="font-bold text-4xl mb-2"> $/199</h1>
+               <p class="text-gray-700 text-2xl">Price Includes GST.</p>
+              </div>
+              
+            </div>
+          </div>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div class="px-20 py-20">
+              <div class="font-bold text-3xl mb-2">Professional</div>
+              <p class="text-gray-700 py-16 text-xl">Start building beautiful websites.</p>
+              <button class="bg-emerald-500 hover:bg-emerald-700  items-center text-white font-bold py-3 px-14 rounded-full">
+                Choose Now
+              </button>
+              <div class=" py-6 ">
+                <h1  class="font-bold text-4xl mb-2">$/299</h1>
+               <p class="text-gray-700 text-2xl">Price Includes GST.</p>
+              </div>
+            </div>
+          </div>
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div class="px-20 py-20">
+              <div class="font-bold text-3xl mb-2">Ultimate</div>
+              <p class="text-gray-700 py-16 text-xl">Start building beautiful websites.</p>
+              <button class="bg-emerald-500 hover:bg-emerald-700   items-center text-white font-bold py-3 px-14 rounded-full">
+                Choose Now
+              </button>
+              <div class=" py-6 ">
+                <h1  class="font-bold text-4xl mb-2">$/399</h1>
+               <p class="text-gray-700 text-2xl">Price Includes GST.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class=" bg-emerald-500  rounded-lg ">
+      <h1 class="font-bold text-white py-20 text-5xl text-center ">Contact Our Sales Team </h1>
+      
+        <p class=" text-white py-6  text-xl text-center"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis eum modi, adipisci facilis.</p>
+        <div class=" flex flex-row items-center justify-center ">
+
+    <!-- registration form -->
+          <form action="" class=" bg-white  rounded-lg outline-double outline-stone-400  ">
+            <div class="mt-4">
+              <div class="flex flex-row space-x-10">
+                <div>
+                  <label class="block" for="Name">Name<label>
+                          <input type="text" placeholder="Name"
+                              class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+              </div>
+              <div class="mt-4">
+                  <label class="block" for="email">Email<label>
+                          <input type="text" placeholder="Email"
+                              class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+              </div>
+
+              </div>
+              <div class="mt-4">
+                  <label class="block">Subject<label>
+                          <input type="subject" placeholder="Subject"
+                              class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+              </div>
+              <div class="mt-4">
+                <label class="block">Message<label>
+                        <input type="message" placeholder="message"
+                            class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
+            </div>
+              </div>
+              
+          </div>
+        </div>
+        <div class="flex flex-row w-full  items-center justify-center">
+          <button  class=" max-w-sm  bg-emerald-500  hover:bg-emerald-600  text-white font-bold text-black-500 py-2 px-4 rounded-lg ">
+            Send Message
+        </button>
+    </div>
+       
+        <div class="flex flex-row items-between justify-between py-40 px-6 pl-9 space-x-10">
+          <div class="">
+           <h1 class="font-bold text-2xl py-3">Email</h1>
+            <p class=" text-slate-500 text-xl ">information@gmail.com</p>
+          
+          </div>
+          <div>
+            <h1 class="font-bold text-2xl py-3">Telephone</h1>
+            <p class=" text-slate-500 text-xl">+(485) 874 249 23</p>
+          </div>
+          <div>
+            <h1 class="font-bold text-2xl py-3"> Business Hours</h1>
+            <p class=" text-slate-500 text-xl">Monday To Friday</p>
+            <p class=" text-slate-500 text-xl">9:00am To 6:00pm (GMT)</p>
+
+          </div>
+        </div>
+
+        
+        <div>
+          <footer class="flex flex-row space-x-16  ">
+            <div>
+              <h1 class="font-bold text-4xl ">Veldron</h1>
+              <p class="text-2xl text-slate-500">2022 © Oliver. Design by ThemeBoss.</p>
+              <p class="font-bold text-3xl">Follow Us</p>
+            </div>
+
+
+            <div>
+              <h1 class="font-bold text-3xl py-5"> Our_Product</h1>
+              <div class="flex flex-col">
+                <span class=" text-slate-500 py-2 text-black-500  text-2xl text-center">Getbootstrap</span>
+              <span class=" text-slate-500 py-2  text-xl text-center">Wordpress</span>
+              <span class=" text-slate-500 py-2  text-xl text-center">Shopify</span>
+              <span class="text-slate-500  py-2 text-xl text-center">React</span>
+              </div>
+            </div>
+      
+            <div>
+              <h1 class="font-bold text-3xl py-3"> Company</h1>
+              <div class="flex flex-col">
+                <span class=" text-slate-500 py-2 text-xl text-center">About</span>
+              <span class="text-slate-500 text-black-500 py-2 text-xl text-center">Terms</span>
+              <span class=" text-slate-500 text-black-500 py-2 text-xl text-center">Privacy_Policy</span>
+              <span class="text-slate-500 text-black-500 py-2 text-xl text-center">Careers
+              </span>
+              </div>
+            </div>
+            <div>
+              <h1 class="font-bold text-3xl py-3"> Services</h1>
+              <div class="flex flex-col">
+                <span class=" text-slate-500  text-black-500 py-2 text-xl text-center">Documentation</span>
+              <span class="text-slate-500 text-black-500  text-xl text-center">Design</span>
+              <span class="text-slate-500 text-black-500  text-xl text-center">Themes</span>
+              <span class="text-slate-500 text-black-500  text-xl text-center">Illustrations</span>
+              <span class="text-slate-500 text-black-500  text-xl text-center">UI Kit</span>
+              </div>
+            </div>
+            <div class="text-center">
+              <h1 class="font-bold text-3xl py-3"> More</h1>
+              <div class="flex flex-col ">
+                <span class=" text-slate-500  text-black-500 py-2 text-xl text-center">Documentation</span>
+              <span class="text-slate-500 text-black-500  text-xl text-center">License</span>
+              <span class="text-slate-500 text-black-500  text-xl text-center">Changelog</span>
+              </div>
+            </div>
+          </footer>
+        </div> 
+    
+    </div>
+    
+  
+   
+    
+    
+
+
