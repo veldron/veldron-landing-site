0ee0c80f611b57530d90b95151510dd026a62463
Vikram Mishra <vikram.mishra420@gmail.com>
2022-07-17 11:45:30 +0200

    github-pages: fix clients

--- src/Clients_section.svelte
+++ src/Clients_section.svelte
@@ -1,19 +1,28 @@
-<div
-  class="flex flex-col justify-center py-5 m-auto mt-5 md:flex-row md:space-x-56">
-  <img
-    class=""
-    src="https://oliver-tailwind.netlify.app/assets/images/clients/1.png"
-    alt="" />
-  <img
-    class=""
-    src="https://oliver-tailwind.netlify.app/assets/images/clients/2.png"
-    alt="" />
-  <img
-    class=""
-    src="https://oliver-tailwind.netlify.app/assets/images/clients/3.png"
-    alt="" />
-  <img
-    class=""
-    src="https://oliver-tailwind.netlify.app/assets/images/clients/4.png"
-    alt="" />
+<div class="flex flex-col items-center justify-center gap-12">
+  <span class=" font-black text-4xl">
+    Our Clients
+  </span>
+  <div
+    class=" flex flex-col gap-y-10 md:flex-row items-center justify-center m-auto md:gap-x-10">
+    <a href="/">
+      <img
+        src="https://oliver-tailwind.netlify.app/assets/images/clients/1.png"
+        alt="" />
+    </a>
+    <a href="/">
+      <img
+        src="https://oliver-tailwind.netlify.app/assets/images/clients/2.png"
+        alt="" />
+    </a>
+    <a href="/">
+      <img
+        src="https://oliver-tailwind.netlify.app/assets/images/clients/3.png"
+        alt="" />
+    </a>
+    <a href="/">
+      <img
+        src="https://oliver-tailwind.netlify.app/assets/images/clients/4.png"
+        alt="" />
+    </a>
+  </div>
 </div>
