4abfe38ef245d4deb5c287d90575423651477852
Vikram Mishra <vikram.mishra420@gmail.com>
2022-07-21 09:51:35 +0200

    github-pages: add calendar button

--- src/Hero_section.svelte
+++ src/Hero_section.svelte
@@ -34,23 +34,33 @@
                 Get Started
               </button> -->
 
-          <div class="relative">
-            <div class="absolute top-4 left-3">
-              <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500" />
-            </div>
-            <input
-              type="text"
-              class="h-14 w-96 pl-10 pr-20 rounded-full shadow-xl
-              border-gray-900 z-0 focus:shadow focus:outline-none"
-              placeholder="Email......." />
-            <div class="absolute top-2 right-2">
-              <button
-                class=" bg-primary-dark hover:bg-primary text-white font-bold
-                py-2 px-5 rounded-full">
-                Contact me
-              </button>
-            </div>
+          <div class="flex flex-col md:flex-row gap-4">
+            <!-- <div class="relative">
+              <div class="absolute top-4 left-3">
+                <i class="fa fa-search text-gray-400 z-20 hover:text-gray-500" />
+              </div>
+              <input
+                type="text"
+                class="h-14 w-96 pl-10 pr-20 rounded-full shadow-xl
+                border-gray-900 z-0 focus:shadow focus:outline-none"
+                placeholder="Email......." />
+              <div class="absolute top-2 right-2">
+                <button
+                  class=" bg-primary-dark hover:bg-primary text-white font-bold
+                  py-2 px-5 rounded-full">
+                  Contact me
+                </button>
+              </div>
+            </div> -->
+          <a target="_blank" href="https://cal.com/insanebaba/short-meet"
+                class="flex h-14 text-center items-center flex-row justify-center w-full gap-4 bg-primary-dark hover:bg-primary text-white font-bold
+                py-2 px-8 rounded-full">
+                Book a meeting <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                </svg>
+              </a>
           </div>
+
         </div>
       </div>
     </div>
